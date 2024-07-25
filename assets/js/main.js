1f6648cb68825e11a8f5426d29bad3ea554d6cba
James Carey <57900176+carey-james@users.noreply.github.com>
2024-07-25 12:51:55 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
-
+function addEventDots(eventDataset, eventColor, eventMap) {
+	eventDataset.forEach((d) => {
+		var circle = L.circle([d.lat, d.long], {
+			color: eventColor,
+			fillColor: eventColor,
+			fillOpacity: 0.5,
+			radius: 50
+		}).addTo(eventMap);
+	});
+}
 
 
 
@@ -7,7 +16,7 @@ async function initMap() {
 	const data_730 = await d3.csv("https://raw.githubusercontent.com/carey-james/DC-Influencers-Events-Map/main/assets/event-datasets/730.csv");
 	const data_yourdcbestie = await d3.csv("https://raw.githubusercontent.com/carey-james/DC-Influencers-Events-Map/main/assets/event-datasets/yourdcbestie.csv");
 
-	var map = L.map('map').setView([38.89,-77.03], 13);
+	var map = L.map('map').setView([38.89,-77.03], 14);
 	
 	L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
     	minZoom: 1,
@@ -15,6 +24,8 @@ async function initMap() {
     	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 		ext: 'png'
 	}).addTo(map);
+
+	addEventDots(data_yourdcbestie, 'blue', map);
 }
 
 initMap();

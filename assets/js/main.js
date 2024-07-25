1be6bd48a42e54eaae76b88c95c34a06a0535323
James Carey <57900176+carey-james@users.noreply.github.com>
2024-07-25 12:11:48 -0400

    Updated CSV gets through D3

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,8 @@
 
 async function initMap() {
 	// Get the events info from the event data files
-	const data_730 = await d3.csv("assets/event-data/730.csv");
-	const data_yourdcbestie = await d3.csv("assets/event-data/yourdcbestie.csv");
+	const data_730 = await d3.csv("https://raw.githubusercontent.com/carey-james/DC-Influencers-Events-Map/main/assets/event-datasets/730.csv");
+	const data_yourdcbestie = await d3.csv("https://raw.githubusercontent.com/carey-james/DC-Influencers-Events-Map/main/assets/event-datasets/yourdcbestie.csv");
 
 	var map = L.map('map').setView([38,-77], 13);
 	

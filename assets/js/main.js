7c86947fb310ac40b252804dc3959c7c50e6e308
James Carey <57900176+carey-james@users.noreply.github.com>
2024-07-25 12:27:17 -0400

    Adjusting base map location

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ async function initMap() {
 	const data_730 = await d3.csv("https://raw.githubusercontent.com/carey-james/DC-Influencers-Events-Map/main/assets/event-datasets/730.csv");
 	const data_yourdcbestie = await d3.csv("https://raw.githubusercontent.com/carey-james/DC-Influencers-Events-Map/main/assets/event-datasets/yourdcbestie.csv");
 
-	var map = L.map('map').setView([38,-77], 13);
+	var map = L.map('map').setView([38.89,-77.03], 13);
 	
 	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     	maxZoom: 19,

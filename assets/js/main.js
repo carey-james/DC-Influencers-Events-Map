62757a506da70a2729b1559a70a76510c5d036e8
James Carey <57900176+carey-james@users.noreply.github.com>
2024-07-25 12:54:38 -0400

    Adjusting map

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,16 +16,18 @@ async function initMap() {
 	const data_730 = await d3.csv("https://raw.githubusercontent.com/carey-james/DC-Influencers-Events-Map/main/assets/event-datasets/730.csv");
 	const data_yourdcbestie = await d3.csv("https://raw.githubusercontent.com/carey-james/DC-Influencers-Events-Map/main/assets/event-datasets/yourdcbestie.csv");
 
-	var map = L.map('map').setView([38.89,-77.03], 14);
+	var map = L.map('map').setView([38.89,-77.03], 10);
 	
 	L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
-    	minZoom: 1,
+    	minZoom: 9,
     	maxZoom: 19,
     	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 		ext: 'png'
 	}).addTo(map);
 
+	addEventDots(data_730, 'red', map);
 	addEventDots(data_yourdcbestie, 'blue', map);
 }
+
 
 initMap();

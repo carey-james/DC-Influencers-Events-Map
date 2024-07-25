b2b4b3f062df9442c523c3c7e2c6bdaaba10057f
James Carey <57900176+carey-james@users.noreply.github.com>
2024-07-25 12:09:16 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,8 @@
 
 async function initMap() {
 	// Get the events info from the event data files
-	const data_730 = await d3.csv("../event-data/730.csv");
-	const data_yourdcbestie = await d3.csv("../event-data/yourdcbestie.csv");
+	const data_730 = await d3.csv("assets/event-data/730.csv");
+	const data_yourdcbestie = await d3.csv("assets/event-data/yourdcbestie.csv");
 
 	var map = L.map('map').setView([38,-77], 13);
 	
@@ -13,9 +13,6 @@ async function initMap() {
     	maxZoom: 19,
     	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	}).addTo(map);
-
-	
-
 }
 
 initMap();
